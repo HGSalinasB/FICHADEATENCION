b430529e79e5e13ec1995b7b15bf7d91e18b8818
Hamlet Salinas <hamletgabriel_@outlook.com>
2022-06-22 15:21:24 -0400

    Agregar archivos de proyecto.

---
+++ RegistroDeFicha/Form1.Designer.cs
@@ -0,0 +1,385 @@
+﻿
+namespace RegistroDeFicha
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblFecha = new System.Windows.Forms.Label();
+            this.txtPaciente = new System.Windows.Forms.TextBox();
+            this.txtCI = new System.Windows.Forms.TextBox();
+            this.cbEspe = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.cbMedico = new System.Windows.Forms.ComboBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.cbGenero = new System.Windows.Forms.ComboBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtEdad = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.lvPacientes = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnRegistrar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(379, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(206, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ficha de Atencion";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(41, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Paciente:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(0, 191);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(142, 23);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Especialidad:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(67, 61);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 23);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Fecha:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Century", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(380, 100);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 23);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "CI:";
+            // 
+            // lblFecha
+            // 
+            this.lblFecha.AutoSize = true;
+            this.lblFecha.Font = new System.Drawing.Font("Century", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFecha.ForeColor = System.Drawing.Color.White;
+            this.lblFecha.Location = new System.Drawing.Point(148, 61);
+            this.lblFecha.Name = "lblFecha";
+            this.lblFecha.Size = new System.Drawing.Size(95, 23);
+            this.lblFecha.TabIndex = 5;
+            this.lblFecha.Text = "lblFecha";
+            // 
+            // txtPaciente
+            // 
+            this.txtPaciente.Location = new System.Drawing.Point(148, 101);
+            this.txtPaciente.Name = "txtPaciente";
+            this.txtPaciente.Size = new System.Drawing.Size(212, 20);
+            this.txtPaciente.TabIndex = 6;
+            this.txtPaciente.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtCI
+            // 
+            this.txtCI.Location = new System.Drawing.Point(426, 101);
+            this.txtCI.Name = "txtCI";
+            this.txtCI.Size = new System.Drawing.Size(121, 20);
+            this.txtCI.TabIndex = 7;
+            // 
+            // cbEspe
+            // 
+            this.cbEspe.FormattingEnabled = true;
+            this.cbEspe.Items.AddRange(new object[] {
+            "Pediatria",
+            "Neonatologia "});
+            this.cbEspe.Location = new System.Drawing.Point(148, 190);
+            this.cbEspe.Name = "cbEspe";
+            this.cbEspe.Size = new System.Drawing.Size(212, 21);
+            this.cbEspe.TabIndex = 8;
+            this.cbEspe.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Century", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(379, 190);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(85, 23);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Medico:";
+            // 
+            // cbMedico
+            // 
+            this.cbMedico.FormattingEnabled = true;
+            this.cbMedico.Items.AddRange(new object[] {
+            "Fernando Cruz (N)",
+            "Gabriel Camargo (P)",
+            "Liz Suarez (P)"});
+            this.cbMedico.Location = new System.Drawing.Point(470, 191);
+            this.cbMedico.Name = "cbMedico";
+            this.cbMedico.Size = new System.Drawing.Size(209, 21);
+            this.cbMedico.TabIndex = 10;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Century", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.White;
+            this.label8.Location = new System.Drawing.Point(574, 98);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(87, 23);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "Genero:";
+            // 
+            // cbGenero
+            // 
+            this.cbGenero.FormattingEnabled = true;
+            this.cbGenero.Items.AddRange(new object[] {
+            "Masculino",
+            "Femenino"});
+            this.cbGenero.Location = new System.Drawing.Point(658, 100);
+            this.cbGenero.Name = "cbGenero";
+            this.cbGenero.Size = new System.Drawing.Size(97, 21);
+            this.cbGenero.TabIndex = 12;
+            this.cbGenero.SelectedIndexChanged += new System.EventHandler(this.cbGenero_SelectedIndexChanged);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Century", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.Color.White;
+            this.label9.Location = new System.Drawing.Point(75, 146);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(67, 23);
+            this.label9.TabIndex = 13;
+            this.label9.Text = "Edad:";
+            // 
+            // txtEdad
+            // 
+            this.txtEdad.Location = new System.Drawing.Point(148, 146);
+            this.txtEdad.Name = "txtEdad";
+            this.txtEdad.Size = new System.Drawing.Size(77, 20);
+            this.txtEdad.TabIndex = 14;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Century", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.ForeColor = System.Drawing.Color.White;
+            this.label10.Location = new System.Drawing.Point(351, 229);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(227, 23);
+            this.label10.TabIndex = 15;
+            this.label10.Text = "Pacientes Registrados";
+            // 
+            // lvPacientes
+            // 
+            this.lvPacientes.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(60)))), ((int)(((byte)(75)))));
+            this.lvPacientes.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lvPacientes.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6,
+            this.columnHeader7});
+            this.lvPacientes.Font = new System.Drawing.Font("Georgia", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lvPacientes.ForeColor = System.Drawing.Color.Teal;
+            this.lvPacientes.FullRowSelect = true;
+            this.lvPacientes.HideSelection = false;
+            this.lvPacientes.Location = new System.Drawing.Point(4, 266);
+            this.lvPacientes.Name = "lvPacientes";
+            this.lvPacientes.Size = new System.Drawing.Size(912, 167);
+            this.lvPacientes.TabIndex = 16;
+            this.lvPacientes.UseCompatibleStateImageBehavior = false;
+            this.lvPacientes.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Paciente";
+            this.columnHeader1.Width = 227;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "CI";
+            this.columnHeader2.Width = 132;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Genero";
+            this.columnHeader3.Width = 72;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Edad";
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Especialidad";
+            this.columnHeader5.Width = 111;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "Medico";
+            this.columnHeader6.Width = 212;
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.Text = "Fecha";
+            this.columnHeader7.Width = 134;
+            // 
+            // btnRegistrar
+            // 
+            this.btnRegistrar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnRegistrar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnRegistrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRegistrar.Font = new System.Drawing.Font("Impact", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegistrar.ForeColor = System.Drawing.Color.White;
+            this.btnRegistrar.ImageAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.btnRegistrar.Location = new System.Drawing.Point(823, 130);
+            this.btnRegistrar.Name = "btnRegistrar";
+            this.btnRegistrar.Size = new System.Drawing.Size(93, 36);
+            this.btnRegistrar.TabIndex = 17;
+            this.btnRegistrar.Text = "REGISTRAR";
+            this.btnRegistrar.UseVisualStyleBackColor = false;
+            this.btnRegistrar.Click += new System.EventHandler(this.btnRegistrar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnSalir.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnSalir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSalir.Font = new System.Drawing.Font("Impact", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir.ForeColor = System.Drawing.Color.White;
+            this.btnSalir.ImageAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.btnSalir.Location = new System.Drawing.Point(823, 179);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(93, 36);
+            this.btnSalir.TabIndex = 18;
+            this.btnSalir.Text = "SALIR";
+            this.btnSalir.UseVisualStyleBackColor = false;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(40)))), ((int)(((byte)(45)))));
+            this.ClientSize = new System.Drawing.Size(920, 449);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnRegistrar);
+            this.Controls.Add(this.lvPacientes);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.txtEdad);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.cbGenero);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.cbMedico);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.cbEspe);
+            this.Controls.Add(this.txtCI);
+            this.Controls.Add(this.txtPaciente);
+            this.Controls.Add(this.lblFecha);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblFecha;
+        private System.Windows.Forms.TextBox txtPaciente;
+        private System.Windows.Forms.TextBox txtCI;
+        private System.Windows.Forms.ComboBox cbEspe;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox cbMedico;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ComboBox cbGenero;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtEdad;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.ListView lvPacientes;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.ColumnHeader columnHeader7;
+        private System.Windows.Forms.Button btnRegistrar;
+        private System.Windows.Forms.Button btnSalir;
+    }
+}
+
---
+++ RegistroDeFicha/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RegistroDeFicha
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblFecha.Text = DateTime.Now.ToString("d");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRegistrar_Click(object sender, EventArgs e)
+        {
+            //capturar datos 
+            string paciente = txtPaciente.Text;
+            string ci = txtCI.Text;
+            string edad = txtEdad.Text;
+            string genero = cbGenero.Text;
+            string espe = cbEspe.Text;
+            string medico = cbMedico.Text;
+
+            //impresion de plantilla
+            ListViewItem fila = new ListViewItem(paciente);
+            fila.SubItems.Add(ci);
+            fila.SubItems.Add(genero);
+            fila.SubItems.Add(edad);
+            fila.SubItems.Add(espe);
+            fila.SubItems.Add(medico);
+            fila.SubItems.Add(DateTime.Now.ToString("d"));
+            lvPacientes.Items.Add(fila);
+
+            //limpiar texbox
+            txtPaciente.Clear();
+            txtCI.Clear();
+            txtEdad.Clear();
+            cbEspe.SelectedIndex = -1;
+            cbGenero.SelectedIndex = -1;
+            cbMedico.SelectedIndex = -1;
+            txtPaciente.Focus();
+        }
+
+        private void cbGenero_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            DialogResult r = MessageBox.Show("Esta seguro de salir?", "planilla", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation);
+            if (r == DialogResult.Yes)
+                this.Close();
+        }
+    }
+}
